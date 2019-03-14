17047ab98835a7a171834dfe33299f557936b8fe
Alison Hill <apreshill@gmail.com>
2019-03-14 12:45:52 -0700

    render themes

---
+++ render_themes.R
@@ -0,0 +1,13 @@
+purrr::walk(
+  .x = as.list(rmarkdown:::themes()),
+  ~ rmarkdown::render(
+    "index.Rmd",
+    output_file = glue::glue("theme_{.}.html"),
+    output_dir = "gallery-theme/",
+    output_options = list(
+      toc = TRUE, 
+      toc_float = TRUE,
+      theme = .x
+    )
+  )
+)
