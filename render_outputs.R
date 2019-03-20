36451b0a79a3d3ce33469c16093ef571f5d96897
Alison Hill <apreshill@gmail.com>
2019-03-20 15:14:14 -0700

    no longer need to specify extensions for rendering multiple output formats (see rstudio/rmarkdown#1551)

--- render_outputs.R
+++ render_outputs.R
@@ -1,13 +1,11 @@
 doc_outputs <- c("html_notebook", "html_document", "pdf_document", 
                  "word_document", "odt_document", "rtf_document", "github_document")
-doc_exts <- c("nb.html", "html", "pdf", "docx", "odt", "rtf", "md")
 
-purrr::walk2(
+purrr::walk(
   .x = doc_outputs,
-  .y = doc_exts,
   ~ rmarkdown::render(
     "input/single.Rmd",
-    output_file = glue::glue("{.x}.{.y}"),
+    output_file = glue::glue("{.}"),
     output_dir = "gallery/outputs/docs/",
     output_format = .,
     params = list(dynamictitle = glue::glue("Texas Housing Prices: `{.}`"))
@@ -15,14 +13,12 @@ purrr::walk2(
 )
 
 slide_outputs <- c("ioslides_presentation", "slidy_presentation", "beamer_presentation", "powerpoint_presentation")
-slide_exts <- c("html", "html", "pdf", "pptx")
 
-purrr::walk2(
+purrr::walk(
   .x = slide_outputs,
-  .y = slide_exts,
   ~ rmarkdown::render(
     "input/single.Rmd",
-    output_file = glue::glue("{.x}.{.y}"),
+    output_file = glue::glue("{.}"),
     output_dir = "gallery/outputs/slides/",
     output_format = .,
     params = list(dynamictitle = glue::glue("Texas Housing Prices: `{.}`"))
